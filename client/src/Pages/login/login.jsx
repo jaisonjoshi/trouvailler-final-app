4cb8b9b1f73ace102e2ee250dfa91656f01f97f4
Sheethal31 <75903042+Ella311@users.noreply.github.com>
2022-11-25 15:34:54 +0530

    updated login

--- client/src/Pages/login/login.jsx
+++ client/src/Pages/login/login.jsx
@@ -1,69 +1,105 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import { AuthContext } from '../context/AuthContext';
-import { useContext } from 'react';
-import {useNavigate} from "react-router-dom"
 import axios from "axios";
-
-const Navbar = () => {
-  //const { user } = useContext(AuthContext);
-
-  const { user, loading, error, dispatch } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  //logout code fetching
-  const handleClick = async (e) => {
-      e.preventDefault();
-      dispatch({ type: "LOGOUT" });
-      try {
-        const res = await axios.get("/auth/logout");
-        localStorage.removeItem("user");
-        if(res){
-        
-        navigate("/");
-  
-                            }
-        
-      } catch (err) {
-        dispatch({ type: "LOGOUT", payload: {message:"logged out"} });
-      }
-    };
-    console.log(user);
-     // {user?():()}
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {AuthContext} from '../components/context/AuthContext'
 
 
 
-  return (
-    <header class="text-blacky-light body-font">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <img src={require('../../Assets/TrouvaillerGreen .png')} alt="" className="px-14" />
-        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a class="text-sm mr-5 text-blacky-light hover:text-evergreen duration-500"><Link className="" to="/">Home</Link></a>
 
-          {user?
-          {/*This section should navigate to user's bid page only after logging in*/}
-          (<a class="text-sm mr-5 text-blacky-light hover:text-evergreen duration-500"><Link className="" to="/bid-status">My bids</Link></a>
-        ):(
-          <a class="text-sm mr-5 text-blacky-light hover:text-evergreen duration-500"><Link className="" to="/login">My bids</Link></a>
-        )}
+const Login = () => {
 
-          <a class="text-sm mr-5 text-blacky-light hover:text-evergreen duration-500"><Link className="" to="/">About Us</Link></a>
-          <a class="text-sm mr-5 text-blacky-light hover:text-evergreen duration-500"><Link className="" to="/">Contact</Link></a>
-        </nav>
-        {user?<div><span>{user.username}</span>
-        <button className="bg-whiteglow text-blacky-dark text-sm border border-none duration-500 px-4 py-2 mx-4 hover:bg-blacky-dark rounded-md hover:text-whiteglow" onClick={handleClick}>Logout</button></div>:(
-        <div className="md:flex my-4">
-           <button className="bg-whiteglow text-blacky-dark text-sm border border-none duration-500 px-4 py-2 mx-4 hover:bg-blacky-dark rounded-md hover:text-whiteglow">
-            <Link className="" to="/login" >Login</Link>
-            </button>
+    const [credentials, setCredentials] = useState({
+        username: undefined,
+        password: undefined,
+      });
+    
+      const { user, loading, error, dispatch } = useContext(AuthContext);
+    
+      const navigate = useNavigate();
+    
+      const handleChange = (e) => {
+        setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+      };
+    
+      const handleClick = async (e) => {
+        e.preventDefault();
+        dispatch({ type: "LOGIN_START" });
+        try {
+          const res = await axios.post("/auth/login", credentials);
+         // if(res.data.isAdmin){//check this code to control user and admin access to login
+          if(res.data){
+            dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+          navigate("/");
+    
+                              }
+          else{
+            dispatch({type:"LOGIN_FAILURE",payload:{message:"Invalid credentials"}})
+          }
+        } catch (err) {
+          dispatch({ type: "LOGIN_FAILURE", payload: {message:"Invalid"} });
+        }
+      };
+      console.log(user);
 
-            <button className="bg-evergreen text-blacky-dark text-sm border border-none duration-500 px-4 py-2 mx-4 hover:bg-blacky-dark rounded-md hover:text-whiteglow">
-            <Link className="" to="/signup">Signup</Link>
-            </button>
-          </div>)}
-      </div>
-    </header>
-  )
+    return (
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+            <div className="">
+
+
+                <div className="mx-14 mt-36 mb-5 grid grid-cols-1 sm:grid-cols-2">
+                    <div className="flex justify-center items-center m-3">
+                        <button className="w-48 flex justify-center items-center text-blacky-light p-3 shadow-lg shadow-gray-500 rounded-md text-xs">
+                            <img src={require('../Assets/Google.png')} alt="Google" className="h-3 w-3 mr-2" />
+                            Sign Up with Google
+                        </button>
+                    </div>
+                    <div className="flex justify-center items-center m-3">
+                        <button className="w-48 flex justify-center items-center text-blacky-light p-3 shadow-lg shadow-gray-500 rounded-md text-xs">
+                            <img src={require('../Assets/Facebook.png')} alt="Facebook" className="h-3 w-3 mr-2" />
+                            Sign Up with Facebook
+                        </button>
+                    </div>
+                </div>
+
+                <p className="text-center text-blacky-light text-sm m-2">- OR -</p>
+
+                <div className="flex flex-col">
+                <input
+            type="text"
+            className="mx-14 p-3 outline-none border border-transparent border-b-blacky-medium hover:border-b-evergreen duration-500"
+            placeholder="username"
+            id="username"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            className="mx-14 p-3 outline-none border border-transparent border-b-blacky-medium hover:border-b-evergreen duration-500"
+            placeholder="password"
+            id="password"
+            onChange={handleChange}
+          />
+          </div>
+
+                <div className="mx-14 my-5">
+                <button
+            disabled={loading}
+            onClick={handleClick}
+            className=" hover:bg-evergreen duration-500 bg-blacky-dark text-whiteglow w-full rounded-md p-2 my-5"
+          >Login</button>
+                {error && <span>{error.message}</span>}        
+                </div>
+
+                <p className="mx-14">New here?
+                    <Link className="" to="/signup">Signup</Link></p>
+            </div>
+
+            <div className="bg-evergreen flex justify-center items-center">
+                <img src={require('../Assets/Trouvailler.png')} alt="" className="" />
+            </div>
+        </div>
+    )
 }
-
-export default Navbar
+export default Login
