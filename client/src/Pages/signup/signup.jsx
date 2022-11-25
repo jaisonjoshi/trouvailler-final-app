de0c362d3b52557e790a7862090d59f3381e859a
Sheethal31 <75903042+Ella311@users.noreply.github.com>
2022-11-25 15:17:15 +0530

    updated signup page

--- client/src/Pages/signup/signup.jsx
+++ client/src/Pages/signup/signup.jsx
@@ -1,7 +1,59 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {AuthContext} from '../components/context/AuthContext'
+
+
+
 
 const Signup = () => {
+
+//register procedure
+const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+    phone: undefined,
+    email: undefined,
+    city:undefined,
+    country:undefined
+  });
+
+  const { user, loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "REGISTER_START" });
+    try {
+      const res = await axios.post("/auth/register", credentials);
+      //if(res.data.isAdmin){//check this code to control user and admin access to login
+      if(res.data){
+      dispatch({ type: "REGISTER_SUCCESS", payload: res.data.details });
+      
+      navigate("/login");
+
+                          }
+      else{
+        dispatch({type:"REGISTER_FAILURE",payload:{message:"Invalid input for Registration!"}})
+      }
+    } catch (err) {
+      dispatch({ type: "REGISTER_FAILURE", payload: {message:"Registration Unsuccessful! Please try again"}});
+    }
+  };
+  console.log(user);
+
+
+
+
+
+
     return (
 
         <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
@@ -27,14 +79,19 @@ const Signup = () => {
                 <p className="text-center text-blacky-light text-sm m-2">- OR -</p>
 
                 <div className="flex flex-col">
-                    <input type="text" className="mx-14 p-3 outline-none border border-t-transparent border-l-transparent border-r-transparent focus:ring-0 focus:ring-offset-0 border-b-blacky-medium hover:border-b-evergreen duration-500" placeholder="Your Name" required />
-                    <input type="email" className="mx-14 p-3 outline-none border border-t-transparent border-l-transparent border-r-transparent focus:ring-0 focus:ring-offset-0 border-b-blacky-medium hover:border-b-evergreen duration-500" placeholder="E-mail" required />
-                    <input type="tel" className="mx-14 p-3 ooutline-none border border-t-transparent border-l-transparent border-r-transparent focus:ring-0 focus:ring-offset-0 border-b-blacky-medium hover:border-b-evergreen duration-500" placeholder="Mobile Number" required />
-                    <input type="password" className="mx-14 outline-none border border-t-transparent border-l-transparent border-r-transparent focus:ring-0 focus:ring-offset-0 border-b-blacky-medium hover:border-b-evergreen duration-500" placeholder="Create Password" required />
+                    <input type="text" className="mx-14 p-3 outline-none border border-t-transparent border-l-transparent border-r-transparent focus:ring-0 focus:ring-offset-0 border-b-blacky-medium hover:border-b-evergreen duration-500" placeholder="Your Name" id="username"required onChange={handleChange}/>
+                    <input type="email" className="mx-14 p-3 outline-none border border-t-transparent border-l-transparent border-r-transparent focus:ring-0 focus:ring-offset-0 border-b-blacky-medium hover:border-b-evergreen duration-500" placeholder="E-mail" id="email" required onChange={handleChange}/>
+                    <input type="tel" className="mx-14 p-3 ooutline-none border border-t-transparent border-l-transparent border-r-transparent focus:ring-0 focus:ring-offset-0 border-b-blacky-medium hover:border-b-evergreen duration-500" placeholder="Mobile Number" id="phone" required onChange={handleChange}/>
+                    <input type="password" className="mx-14 outline-none border border-t-transparent border-l-transparent border-r-transparent focus:ring-0 focus:ring-offset-0 border-b-blacky-medium hover:border-b-evergreen duration-500" placeholder="Create Password" id="password" required onChange={handleChange}/>
+                    <input type="text" className="mx-14 p-3 ooutline-none border border-t-transparent border-l-transparent border-r-transparent focus:ring-0 focus:ring-offset-0 border-b-blacky-medium hover:border-b-evergreen duration-500" placeholder="City" id="city" required onChange={handleChange}/>
+                    <input type="text" className="mx-14 outline-none border border-t-transparent border-l-transparent border-r-transparent focus:ring-0 focus:ring-offset-0 border-b-blacky-medium hover:border-b-evergreen duration-500" placeholder="Country" id="country" required onChange={handleChange}/>
+                
                 </div>
 
                 <div className="mx-14 my-5">
-                    <button className=" hover:bg-evergreen duration-500 bg-blacky-dark text-whiteglow w-full rounded-md p-2 my-5">Create Account</button>
+                    <button className=" hover:bg-evergreen duration-500 bg-blacky-dark text-whiteglow w-full rounded-md p-2 my-5" disabled={loading}
+                        onClick={handleClick}>Create Account</button>
+                           {error && <span>{error.message}</span>}   
                 </div>
 
                 <p className="mx-14">Already have an account?
